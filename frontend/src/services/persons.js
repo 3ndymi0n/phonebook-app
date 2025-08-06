0291a09b3f68e65942cf13d825a97348dfe5e429
3ndymi0n <endymi0n@systemdecay.info>
2025-08-06 17:30:33 +1000

    updated baseUrl for render.com backend

--- frontend/src/services/persons.js
+++ frontend/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'https://phonebook-app-0rey.onrender.com/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
